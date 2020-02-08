1312ea4f9e0f715f4bf1ae2c52aa68af07abb3bc
Dustin Myers <dustin@lambdaschool.com>
2020-02-08 14:26:41 -0700

    Fix heading width

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -6,7 +6,7 @@ const Nav = () => (
   <header className={styles.header}>
     <img src="/SW-logo.png" width="120" />
     <h1>Star Wars Characters</h1>
-    <div />
+    <div style={{ width: "120px" }} />
   </header>
 );
 
