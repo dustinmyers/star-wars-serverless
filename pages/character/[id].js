a6313957520e0b25b767e713078349b5eaf526a2
Dustin Myers <dustin@lambdaschool.com>
2020-02-08 18:07:40 -0700

    Finish solution

--- pages/character/[id].js
+++ pages/character/[id].js
@@ -1,15 +1,34 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import fetch from "unfetch";
+import useSWR from "swr";
 
 import styles from "./Character.module.css";
 
+async function fetcher(path) {
+  const res = await fetch(path);
+  const json = await res.json();
+  return json;
+}
+
 const Character = () => {
+  const [character, setCharacter] = useState();
   const router = useRouter();
   const { id } = router.query;
+  console.log(id);
+  const { data } = useSWR(`/api/get-character-by-id?id=${id}`, fetcher);
 
+  useEffect(() => {
+    if (data && !data.error) {
+      setCharacter(data.character);
+    }
+  }, [data]);
+  if (!character) return <h3>Fetching character data...</h3>;
   return (
     <main className="App">
       <article className={styles.characterPage}>
-        <h1>Follow the tutorial to build out this page 🚀</h1>
+        <img src={character.thumbnail} alt={character.name} />
+        <h1>{character.name}</h1>
       </article>
     </main>
   );
